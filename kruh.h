ac973d36870bbc4365422fb3796c5c72ac33d5e3
andrewp-dot <ado.ponechal@gmail.com>
2020-11-24 13:12:44 +0100

    last commit

--- kruh.h
+++ kruh.h
@@ -9,8 +9,11 @@
      static constexpr float PI=3.14; //staticka premenna, constexpr - constant expresion - konstanty retazec; nie je sucast objektu, patri len danej triede
  public:
      static Kruh getMax(Kruh pole[],int pocet);
+     static void vymenKruhy(Kruh *prvy, Kruh *druhy);
+     static void vymenKruhy(Kruh &prvy, Kruh &druhy);
      Kruh(float mojPolomer, char mojNazov);
      Kruh();
+     static float getFloat(bool noZero = false, bool noNegative = false);
      explicit Kruh(float mojPolomer);
      explicit Kruh(char mojNazov); //explicit, pretoze mame len 1 parameter a nechceme, aby C++ robilo automaticku konverziu
      float getPolomer() const; //ked metoda nemeni objekt, dajme vediet kompilatoru - "const"
@@ -19,11 +22,71 @@
      void setNazov(char mojNazov);
      float getObvod() const;
      float getObsah() const;
-     void vypisKruh() const;
-     bool jeVacsi(Kruh k) const;
-     Kruh pricitajKruh(Kruh k) const;
-     Kruh vydel(float n) const;
-     Kruh pripocitaj(float cislo) const;
+     bool operator>(const Kruh & dalsiKruh) const;
+     bool operator<(const Kruh & dalsiKruh) const;
+     Kruh operator+(const Kruh & inyKruh) const;
+     Kruh operator+(float cislo) const;
+     Kruh operator-(const Kruh & inyKruh) const;
+     Kruh operator/(float cislo) const;
+     Kruh operator*(float cislo) const;
+     void bubbleSort(Kruh pole[], int pocet, bool desc);
+     static void vypisKruhy(const Kruh arr[], int arrLength);
+     friend Kruh operator+(float cislo, const Kruh & other);
+     friend Kruh operator*(float cislo, const Kruh & other); //priatelska metoda - nie je prvkom triedy, ale ma pristup k parametrom triedy
+     friend std::ostream & operator<<(std::ostream & os, const Kruh & other);
+     friend std::istream & operator>>(std::istream & is, Kruh & other);
+     const Kruh & operator++(); //prefixovy operator
+     Kruh operator++(int nepotrebna); //postfixovy operato; nepotrebna - symbol, ktory odlisuje prefixovy operator od postfixu
+     //vracat iba klasicky kruh pri postfixe
+     const Kruh & operator--();
+     Kruh operator--(int nepotrebna);
+     const Kruh & operator+=(float cislo);
+     //friend Kruh operator+=(Kruh other, Kruh other2);
+     const Kruh & operator-=(float cislo);
+     const Kruh & operator/=(float cislo);
+     const Kruh & operator*=(float cislo);
+     static Kruh * generujKruhy( int arrayLength);
+
+     class noNumber
+     {
+     private:
+         const char * msg;
+     public:
+         noNumber(const char * sprava):msg(sprava){};
+         void getMsg() const;
+     };
+
+     class noZero
+     {
+     private:
+         const char * msg1;
+     public:
+         noZero(const char * sprava1):msg1(sprava1){};
+         void getMsg1() const;
+     };
+
+     class noNegative
+     {
+     private:
+         const char * msg2;
+     public:
+         noNegative(const char * sprava2):msg2(sprava2){};
+         void getMsg2() const;
+
+     };
+
+     class streamError
+     {
+     private:
+         const char * streamErrorMsg;
+     public:
+         streamError(const char * streamErrorSprava):streamErrorMsg(streamErrorSprava){};
+         void getStreamErrorMsg() const;
+     };
+
+
  };
+
+
 
  #endif
--- main.cpp
+++ main.cpp
@@ -1,16 +1,91 @@
 #include <iostream>
 #include "kruh.h"
+#include <limits>
+#include <time.h>
+#include <fstream>
+
+int compFromH(const void * a1,const void *a2);
+int compFromL(const void * a1,const void *a2);
+int compName(const void * a1,const void *a2);
+void generujDoSuboru(int pocetKruhov, std::ofstream  &fout);
+void nacitajZoSuboru(int arrLength, std::ifstream &ffin);
+
+//vygenerovat do suboru 250 000 kruhov, precitat ich zo suboru a vlozit do pola a potom triedit toto pole pomocou qsortu aj bubblesortu a porovnat cas
 
 int main() {
 
-    /*Kruh Prvy(2, 'e');
-    Kruh Dalsi(77,'x');
-    Kruh Druhy(8, 'm');*/
+    srand(time(NULL));
+    /*Kruh & odkazKruh = prvyKruh;
+     & neznamena miesto v pamati, ale odkazuje na miesto v pamati obejktu prvyKruh, pri objektoch pouzivat odkazy - je to rychlejsie
+    float cislo = Kruh::getFloat();*/
 
-    Kruh Vysledny(0, 's');
-    Kruh Kruhy[3] = {{5, 'm'}, {7 , 'n'} , {10 , 'o'}};
+    //std::ofstream ffout;
 
-    //int sum=0;
+
+    try
+    {
+        std::ofstream fout("zapis.txt"); //deklaracia objektu s otvorenim suboru
+        //ffout.open("zapis.txt"); //otvorenie suboru metodou open
+        if(!fout.is_open())
+        {
+            throw Kruh::streamError("Pri praci so suborom sa nieco dojebalo.");
+        };
+        fout.close();
+
+    }
+    catch (const Kruh::streamError &ex)
+    {
+        ex.getStreamErrorMsg();
+        return 1;
+    }
+
+    std::ofstream ffout;
+    std::ifstream fin;
+    generujDoSuboru(1, ffout);
+
+    try
+    {
+        fin.open("zapis.txt");
+        if(!fin.is_open())
+        {
+            throw Kruh::streamError("Pri praci so suborom sa vyskytla chyba");
+        }
+        Kruh k;
+        while(fin>>k)
+        {
+            std::cout<<k;
+        }
+        fin.close();
+    }
+    catch (const Kruh::streamError &ex)
+    {
+        ex.getStreamErrorMsg();
+        return 1;
+    }
+
+
+    /*for(int i = 0;i<100;++i)
+    {
+        srand(time(NULL));
+
+    }*/
+
+   /* Kruh a(5,'a');
+    Kruh b(10,'b');
+    Kruh::vymenKruhy(&a,&b);
+    Kruh::vymenKruhy(a,b);
+
+    Kruh c = a+b; //c = a.operator+(b); - preklad kompilatora - pretazenie operatora +
+
+    std::cout << (a<b) << std::endl;
+    std::cout << c;
+
+
+
+    //Kruh D = c*10; //c.operator*(10)
+    ///Kruh E = 10*c: //10.operator*(c)
+
+    int sum=0;
 
     //for(int i=0; i<3; i++)
     //{
@@ -18,32 +93,46 @@ int main() {
     //}
     for(auto i: Kruhy)
     {
-        Vysledny=Vysledny.pricitajKruh(i);
+        Vysledny+=i;
     }
-    Vysledny.vypisKruh();
-    Vysledny.vydel(3).vypisKruh();
-    Vysledny.pripocitaj(13).vypisKruh();
-    Kruh::getMax(Kruhy , 3).vypisKruh();
+    Kruh spoluNew(8, 'l');
+    for(auto i:Kruhy)
+    {
+        spoluNew += i;
+    }
+
+
+    Kruh *pKruh = new Kruh(8,'s');
+    int *pole =new int[10]; //to iste ako v cecku (int *)malloc(sizeof(int)*10);
+    for(int i=0;i<*pole+10;++i)
+    {
+        *(pole + i)=i;
+        std::cout <<  pole[i] << std::endl;
+    };
+
+    for(auto i:Kruhy)
+    {
+        *pKruh += i;
+    }
+    pKruh->vypisKruh();*/
 
     return 0;
 }
 
 Kruh::Kruh() {
-    std::cout << "Zadaj polomer: ";
-    std::cin>>polomer;
-    std::cout<<"Zadaj nazov: ";
-    std::cin>>nazov;
+   polomer = 1;
+   nazov = 's';
 }
 
 Kruh::Kruh(char mojNazov):nazov(mojNazov) { //inicializacia je ryhclejsia!
     //nazov=mojNazov;
-    std::cout << "Zadaj polomer: ";
+    std::cout << "Zadaj polomer kruhu: ";
     std::cin>>polomer;
 }
 
 Kruh::Kruh(float mojPolomer):polomer(mojPolomer) {
     //polomer=mojPolomer;
-    std::cout<<"Zadaj nazov: ";
+    std::cout<<"Zadaj nazov kruhu: ";
     std::cin>>nazov;
 }
 
@@ -52,8 +141,16 @@ Kruh::Kruh(float mojPolomer, char mojNazov):polomer(mojPolomer),nazov(mojNazov) 
  //   nazov=mojNazov;
 }
 
-bool Kruh::jeVacsi(Kruh k) const {
-    return polomer > k.polomer;
+
+
+bool Kruh::operator>(const Kruh & dalsiKruh) const
+{
+    return polomer > dalsiKruh.polomer;
+}
+
+bool Kruh::operator<(const Kruh & dalsiKruh) const
+{
+    return polomer < dalsiKruh.polomer;
 }
 
 float Kruh::getPolomer() const {
@@ -81,34 +178,280 @@ float Kruh::getObsah() const {
     return PI*polomer*polomer;
 }
 
-void Kruh::vypisKruh() const {
-    std::cout<< "Kruh " << nazov << " ma polomer " << polomer << std::endl;
-}
-
-Kruh Kruh::pricitajKruh(Kruh k) const {
-    return Kruh(polomer+k.polomer, this->jeVacsi(k) ? nazov : k.nazov);
-}
-
-Kruh Kruh::vydel(float n) const {
-    return Kruh(polomer/n, nazov);
-}
-
-Kruh Kruh::pripocitaj(float cislo) const
-{
-    return {polomer+cislo,nazov};
-}
-
 Kruh Kruh::getMax(Kruh pole[],int pocet)
 {
     Kruh max = pole[0];
 
     for(int i=1;i<pocet;i++)
     {
-        if(pole[i].jeVacsi(max))
+        if(pole[i]>max)
         {
             max=pole[i];
         }
     }
     return max;
 }
+
+Kruh Kruh::operator+(const Kruh & inyKruh) const
+{
+    return {polomer+inyKruh.polomer, (polomer>inyKruh.polomer) ? nazov : inyKruh.nazov };
+}
+
+void Kruh::vymenKruhy(Kruh *prvy, Kruh *druhy)
+{
+    Kruh temp = *prvy;
+    *prvy = *druhy;
+    *druhy = temp;
+}
+
+void Kruh::vymenKruhy(Kruh &prvy, Kruh &druhy)
+{
+    Kruh temp = prvy;
+    prvy = druhy;
+    druhy = temp;
+}
+
+Kruh Kruh::operator/(float cislo) const
+{
+    return{ polomer/cislo, nazov};
+}
+Kruh Kruh::operator-(const Kruh & inyKruh) const
+{
+    return { polomer>=inyKruh.polomer ? polomer - inyKruh.polomer : 1, nazov> inyKruh.nazov ? nazov : inyKruh.nazov};
+}
+
+Kruh Kruh::operator*(float cislo) const
+{
+    return{ polomer*cislo, nazov};
+}
+
+const Kruh & Kruh::operator++() //vzdy vratit konstatny odkaz
+{
+    ++polomer;
+    return *this;
+}
+
+const Kruh &Kruh::operator--() {
+    polomer = (polomer-1 <=0)? 1 : polomer -1;
+    return *this;
+}
+
+Kruh Kruh::operator++(int nepotrebna)
+{
+    Kruh Tmp = *this;
+    ++polomer;
+    return Tmp; //nemozno vratit odkaz na premennu v zasobniku Tmp, lebo premenna pri skonceni funkcie zanika
+}
+
+Kruh Kruh::operator--(int nepotrebna)
+{
+    Kruh Tmp  = *this;
+    polomer = (polomer-1 <=0)? 1 : polomer -1;
+    return Tmp;
+}
+
+const Kruh & Kruh::operator-=(float cislo)
+{
+    polomer = (polomer -= cislo) <= 0? 1: polomer-cislo;
+    return *this;
+}
+
+const Kruh & Kruh::operator+=(float cislo)
+{
+    polomer += cislo;
+    return *this;
+}
+
+const Kruh & Kruh::operator/=(float cislo)
+{
+    polomer /= cislo;
+    return *this;
+}
+
+const Kruh & Kruh::operator*=(float cislo)
+{
+    polomer *= cislo;
+    return *this;
+}
+
+Kruh operator*(float cislo, const Kruh & other)
+{
+    //return {other.polomer*cislo, other.nazov};
+    return other*cislo; //funguje iba ak mame pretazeny objekt * cislo
+}
+
+Kruh operator+(float cislo, const Kruh & other)
+{
+    return other + cislo;
+}
+
+
+Kruh Kruh::operator+(float cislo) const
+{
+    return{polomer+cislo,nazov};
+}
+
+std::ostream & operator<<(std::ostream & os, const Kruh & other)
+{
+    os<< "Kruh "<< other.nazov <<" ma polomer "<<other.polomer<<"." <<std::endl;
+    return os;
+}
+
+std::istream & operator>>(std::istream & is, Kruh & other)
+{
+    std::cout << "Zadaj polomer kruhu: " << std::endl;
+    is>>other.polomer;
+    std::cout << "Zadaj nazov kruhu: " << std::endl;
+    is>>other.nazov;
+
+    return is;
+}
+float Kruh::getFloat(bool noZero, bool noNegative)
+{
+    float tmp;
+    while(true)
+    {
+        try
+        {
+            std::cout<< "Zadaj realne cislo: ";
+            if(!(std::cin>>tmp))
+            {
+                throw Kruh::noNumber("Nebolo zadane cislo. ");
+            }
+            if(noZero == true and tmp == 0)
+            {
+                throw Kruh::noZero("Cislo je rovne nule. Zadaj dalsie cislo: ");
+            }
+            if(noNegative ==true and tmp <=0)
+            {
+                throw Kruh::noNumber("Cislo je zaporne. Zadaj dalsie cislo: ");
+            }
+
+        }
+        catch (const noNumber &ex)
+        {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            ex.getMsg();
+            continue;
+        }
+        return tmp;
+    }
+
+}
+
+
+void Kruh::noNumber::getMsg() const
+{
+    std::cout<<msg<<std::endl;
+}
+
+void Kruh::noZero::getMsg1() const
+{
+    std::cout<<msg1<<std::endl;
+}
+
+void Kruh::noNegative::getMsg2() const
+{
+    std::cout<<msg2<<std::endl;
+}
+
+void Kruh::streamError::getStreamErrorMsg() const
+{
+    std::cout<<streamErrorMsg<<std::endl;
+}
+
+
+Kruh * Kruh::generujKruhy(int arrayLength)
+{
+    Kruh *newArr = new Kruh[arrayLength];
+
+    srand(time(NULL));
+    float randPolomer;
+    char randNazov;
+
+    for(int i=0;i<arrayLength; i++)
+    {
+        randPolomer=rand() % 100;
+        randNazov = rand() % 25 + 97;
+
+        *(newArr + i)={  randPolomer,  randNazov};
+    }
+    return newArr;
+}
+
+void Kruh::bubbleSort(Kruh pole[], int pocet, bool desc) {
+    Kruh tmp;
+    bool zoradene;
+    for (int i = 0; i < pocet - 1; ++i) {
+        zoradene = true;
+        for (int j = 0; j < pocet - 1; ++j) {
+            if (pole[j] > pole[j + 1]) {
+                tmp = pole[j];
+                pole[j] = pole[j + 1];
+                pole[j + 1] = tmp;
+                zoradene = false;
+            }
+        }
+
+        if (zoradene) {
+            break;
+        }
+    }
+}
+
+int compFromL(const void * a1,const void *a2)
+{
+    Kruh * p1 = (Kruh *)a1;
+    Kruh * p2 = (Kruh *)a2;
+    return p1->getPolomer()-p2->getPolomer();
+}
+
+int compFromH(const void * a1,const void *a2)
+{
+    Kruh * p1 = (Kruh *)a1;
+    Kruh * p2 = (Kruh *)a2;
+    return p2->getPolomer()-p1->getPolomer();
+}
+
+int compName(const void * a1,const void *a2)
+{
+    Kruh * p1 = (Kruh *)a1;
+    Kruh * p2 = (Kruh *)a2;
+    return p1->getNazov()-p2->getNazov();
+}
+
+void Kruh::vypisKruhy(const Kruh arr[], int arrLength)
+{
+    for(int i=0;i<arrLength;++i)
+    {
+        std::cout<< *(arr + i)<<std::endl;
+    }
+}
+
+void generujDoSuboru(int pocetKruhov, std::ofstream &fout)
+{
+    fout.open("zapis.txt");
+    for(int i =0; i<pocetKruhov; ++i)
+    {
+
+        char name = rand() % 24 +97;
+        fout<< name << " " << rand() % 100 << std::endl;
+    }
+    fout.close();
+}
+
+void nacitajZoSuboru(int arrLength, std::ifstream &ffin)
+{
+    Kruh *pole = new Kruh[arrLength];
+    for(int i = 0; i<arrLength; i++)
+    {
+        ffin>>*(pole + i);
+        std::cout<<*(pole+i);
+    }
+}
+
+
+
+
 
